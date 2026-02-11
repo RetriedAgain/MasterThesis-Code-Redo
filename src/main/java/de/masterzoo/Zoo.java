93c3b60f41d7222b758880506138614efd78758c
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-02-11 14:37:49 +0100

    Change Method Signature #2: Change Return Type

--- src/main/java/de/masterzoo/Zoo.java
+++ src/main/java/de/masterzoo/Zoo.java
@@ -84,11 +84,12 @@ public class Zoo {
 
 	/**
 	 * This method collects the rent of each company given in the list of partnercompanies for a given YearMonth.
+	 *
 	 * @param partnerCompanies List of partner companies that the rent is to be collected from.
-	 * @param yearMonth The yearmonth in which the rentcollection is to be done.
+	 * @param yearMonth        The yearmonth in which the rentcollection is to be done.
 	 * @return The total amount of collectedRent.
 	 */
-	public int collectRentInYearMonth(List<Company> partnerCompanies, YearMonth yearMonth) {
+	public Integer collectRentInYearMonth(List<Company> partnerCompanies, YearMonth yearMonth) {
 		HashMap<YearMonth, Integer> collectedRentInYearMonth = new HashMap<>();
 		int collectedRent = 0;
 		for (Company partnerCompany : partnerCompanies) {
