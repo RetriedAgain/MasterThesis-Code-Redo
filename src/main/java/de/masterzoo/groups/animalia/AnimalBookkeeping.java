d7e36c92870eaf0672893b391dbac384c938d9c5
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-02-18 14:08:17 +0100

    Convert To Instance Method #2: Protected Visibility

--- src/main/java/de/masterzoo/groups/animalia/AnimalBookkeeping.java
+++ src/main/java/de/masterzoo/groups/animalia/AnimalBookkeeping.java
@@ -18,9 +18,9 @@ public final class AnimalBookkeeping {
 	private static Map<Animal, Animal> polyamorousGroups = new HashMap<>();
 
 	public static void equalOutStatsForBookkeeping() {
-		Animal.createTempAnimal();
-		Animal.createTempAnimal();
-		Animal.createTempAnimal();
+		new Animal().createTempAnimal();
+		new Animal().createTempAnimal();
+		new Animal().createTempAnimal();
 	}
 
 	public static List<Animal> getAllAnimals() {
