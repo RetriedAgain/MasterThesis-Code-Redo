6a8ab2bf33bf8f1110abf9a7fa1dc48ae0d3539f
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-02-21 11:10:28 +0100

    Extract Functional Variable #10: Special/Potential-TF: Stop refactor after Introduce Functional Variable dialog (the conversion into a Consumer) and do not actually introduce the variable

--- src/main/java/de/masterzoo/DummyCalculationForExtractRefactorings.java
+++ src/main/java/de/masterzoo/DummyCalculationForExtractRefactorings.java
@@ -47,6 +47,8 @@ public class DummyCalculationForExtractRefactorings {
 		map.put(3, 3);
 		String suffixString = " end of pair.";
 
-		map.forEach((n, m) -> { System.out.println(prefixString + n + m + suffixString); });
+		((BiConsumer<HashMap<Integer, Integer>, String>) (map1, suffixString1) -> map1.forEach((n, m) -> {
+			System.out.println(prefixString + n + m + suffixString1);
+		})).accept(map, suffixString);
 	}
 }
