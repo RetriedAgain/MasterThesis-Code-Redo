75cbd8a2f74c4e85e0ca0d5c6edc658b0192cbc5
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-02-21 11:05:01 +0100

    Extract Functional Variable #8: Declare as var type

--- src/main/java/de/masterzoo/DummyCalculationForExtractRefactorings.java
+++ src/main/java/de/masterzoo/DummyCalculationForExtractRefactorings.java
@@ -47,6 +47,9 @@ public class DummyCalculationForExtractRefactorings {
 		map.put(3, 3);
 		String suffixString = " end of pair.";
 
-		map.forEach((n, m) -> { System.out.println(prefixString + n + m + suffixString); });
+		var hashMapStringBiConsumer = (BiConsumer<HashMap<Integer, Integer>, String>) (map1, suffixString1) -> map1.forEach((n, m) -> {
+			System.out.println(prefixString + n + m + suffixString1);
+		});
+		hashMapStringBiConsumer.accept(map, suffixString);
 	}
 }
