4e5e9fbae037bb2c681fbe132a3419f150e3968b
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-02-23 09:12:45 +0100

    Ready Extract Interface:
    - add JavaDog
    - add method doPuppyEyes() to avoid compiler-errors

--- src/main/java/de/masterzoo/groups/animalia/canines/Labrador.java
+++ src/main/java/de/masterzoo/groups/animalia/canines/Labrador.java
@@ -11,14 +11,40 @@ import java.util.ArrayList;
  * It's a labrador. They are very cute.
  */
 public class Labrador extends Canine {
+	/**
+	 * Judges if this Labrador can actually be trained.
+	 */
 	final static boolean TRAINABLE = true;
 
+	/**
+	 * The human companion of the Labrador. This is intended to be the most sociable person for this dog, to which
+	 * this dog connects the most.
+	 */
 	Employee humanCompanion;
 
+	/**
+	 * Chained constructor for when there is no human companion.
+	 * @param GENDER Gender of the Labrador
+	 * @param name Name of the Labrador
+	 * @param age Age of the Labrador
+	 * @param speed Max. speed of the Labrador.
+	 * @param weight Weight of the Labrador.
+	 * @param reproductionStrategy How a Labrador reproduces.
+	 */
 	public Labrador(Gender GENDER, String name, int age, int speed, int weight, ReproductionStrategy reproductionStrategy) {
 		this(GENDER, name, age, speed, weight, reproductionStrategy, null);
 	}
 
+	/**
+	 * Constructor for Labrador. Differs from canine in that a human companion can be selected.
+	 * @param GENDER Gender of the Labrador
+	 * @param name Name of the Labrador
+	 * @param age Age of the Labrador
+	 * @param speed Max. speed of the Labrador.
+	 * @param weight Weight of the Labrador.
+	 * @param reproductionStrategy How a Labrador reproduces.
+	 * @param humanCompanion Human Companion of the Labrador
+	 */
 	public Labrador(Gender GENDER, String name, int age, int speed, int weight, ReproductionStrategy reproductionStrategy, Employee humanCompanion) {
 		super(GENDER, name, age, speed, weight, reproductionStrategy);
 		this.humanCompanion = humanCompanion;
@@ -29,6 +55,13 @@ public class Labrador extends Canine {
 	 */
 	public void doTrick() {
 		System.out.println("Flips thanks to " + humanCompanion.getName() + "!");
+	}
+
+	/**
+	 * A natural talent of labradors.
+	 */
+	public void doPuppyEyes() {
+		System.out.println("*blink*");
 	}
 
 	public Employee getHumanCompanion() {
