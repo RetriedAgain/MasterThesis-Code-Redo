53564d3f20cbc54e07202c28df36d545c1ad3aa7
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-02-23 10:38:37 +0100

    Extract Interface #5: Select single method to form interface and do not make it abstract

---
+++ src/main/java/de/masterzoo/groups/animalia/canines/T.java
@@ -0,0 +1,10 @@
+package de.masterzoo.groups.animalia.canines;
+
+public interface T {
+	/**
+	 * A natural talent of labradors.
+	 */
+	default void doPuppyEyes() {
+		System.out.println("*blink*");
+	}
+}
