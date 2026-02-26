3056c96f6a95dda9a552236dd6f70d4622fdc545
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-02-26 14:33:12 +0100

    Extract Superclass #4: Extract no members to form superclass

---
+++ src/main/java/de/masterzoo/groups/animalia/canines/D.java
@@ -0,0 +1,10 @@
+package de.masterzoo.groups.animalia.canines;
+
+import de.Gender;
+import de.masterzoo.groups.animalia.reproduction.ReproductionStrategy;
+
+public class D extends Canine {
+	public D(Gender GENDER, String name, int age, int speed, int weight, ReproductionStrategy reproductionStrategy) {
+		super(GENDER, name, age, speed, weight, reproductionStrategy);
+	}
+}
