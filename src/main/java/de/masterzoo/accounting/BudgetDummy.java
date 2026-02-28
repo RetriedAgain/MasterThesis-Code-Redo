1c92243c901534715dc8366427717694fe59309c
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-02-28 10:39:59 +0100

    Showcase bug: int should still be boxed as Integer

--- src/main/java/de/masterzoo/accounting/BudgetDummy.java
+++ src/main/java/de/masterzoo/accounting/BudgetDummy.java
@@ -2,12 +2,17 @@ package de.masterzoo.accounting;
 
 import de.masterzoo.Zoo;
 
+import java.security.PrivilegedAction;
 import java.time.YearMonth;
 
 public class BudgetDummy {
 
 	public BudgetDummy(String name, YearMonth yearMonth, double moneyAllocated) {
 		Zoo zoo = new Zoo();
-		zoo.calculateInitialBudgetDistribution();
+		zoo.calculateInitialBudgetDistribution(new PrivilegedAction<Integer>() {
+			public int run() {
+				return 200_000;
+			}
+		});
 	}
 }
