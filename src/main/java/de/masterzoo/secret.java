771282b16317fc433ca9170738ed3b63092cef51
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-03-02 14:48:24 +0100

    Extract Delegate #8: Visibility public

---
+++ src/main/java/de/masterzoo/secret.java
@@ -0,0 +1,12 @@
+package de.masterzoo;
+
+import org.jetbrains.annotations.NonNls;
+
+public class secret {
+    @NonNls
+    public static final String WELCOME_GREETING = "Welcome to the Zoo!";
+
+    public static void welcomeCustomers() {
+        System.out.println(WELCOME_GREETING);
+    }
+}
