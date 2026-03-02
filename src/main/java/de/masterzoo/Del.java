be76ca3177b6fca1885e826a96674620a87d822b
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-03-02 16:10:16 +0100

    Extract Delegate #13: Special: Extract Delegate, but have delegate name start with capital letter

---
+++ src/main/java/de/masterzoo/Del.java
@@ -0,0 +1,12 @@
+package de.masterzoo;
+
+import org.jetbrains.annotations.NonNls;
+
+public class Del {
+    @NonNls
+    static final String WELCOME_GREETING = "Welcome to the Zoo!";
+
+    static void welcomeCustomers() {
+        System.out.println(WELCOME_GREETING);
+    }
+}
