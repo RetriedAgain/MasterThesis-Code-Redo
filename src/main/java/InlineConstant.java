4bc7ff528906134f6bdd99957e23ab2a1246a323
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-03-11 16:43:43 +0100

    Inline Constant #1: int-constant: inline all usages, remove the field

--- src/main/java/InlineConstant.java
+++ src/main/java/InlineConstant.java
@@ -1,29 +1,28 @@
 public class InlineConstant {
-	final static int INLINE_CONSTANT = 5;
 	final static int[] INT_ARRAY = new int[5];
 
 
 	public static void min() {
-		System.out.println(INLINE_CONSTANT);
+		System.out.println(5);
 	}
 
 	public static void ifCond() {
-		if (INLINE_CONSTANT == 5) {
+		if (5 == 5) {
 			System.out.println("Good job");
 		}
 	}
 
 	public static void ifCondUnder() {
 		if (true) {
-			System.out.println(INLINE_CONSTANT);
+			System.out.println(5);
 		}
 	}
 
 
 	public static void if_else_if_cond(int paramVariable) {
-		if ((INLINE_CONSTANT + paramVariable) == 5) {
+		if ((5 + paramVariable) == 5) {
 			System.out.println("Good job");
-		} else if(INLINE_CONSTANT == 5) {
+		} else if(5 == 5) {
 			System.out.println("Bad job");
 		}
 	}
@@ -32,12 +31,12 @@ public class InlineConstant {
 		if (false) {
 			System.out.println("Good job");
 		} else if(true) {
-			System.out.println(INLINE_CONSTANT);
+			System.out.println(5);
 		}
 	}
 
 	public static void if_else_cond(int paramVariable) {
-		if (INLINE_CONSTANT == 5) {
+		if (5 == 5) {
 			System.out.println("Good job");
 		} else {
 			System.out.println("Bad job");
@@ -48,25 +47,25 @@ public class InlineConstant {
 		if (false) {
 			System.out.println("Good job");
 		} else {
-			System.out.println(INLINE_CONSTANT);
+			System.out.println(5);
 		}
 	}
 
 	public static void while_cond() {
-		while (INLINE_CONSTANT == 5) {
+		while (5 == 5) {
 			System.out.println("Good job");
 		}
 	}
 
 	public static void while_under() {
 		while (true) {
-			System.out.println(INLINE_CONSTANT);
+			System.out.println(5);
 			break;
 		}
 	}
 
 	public static void switch_cond() {
-		switch (INLINE_CONSTANT) {
+		switch (5) {
 			case 5:
 				System.out.println("Good job");
 		}
@@ -74,7 +73,7 @@ public class InlineConstant {
 
 	public static void switch_case_cond(int paramVariable) {
 		switch (paramVariable) {
-			case INLINE_CONSTANT:
+			case 5:
 				System.out.println("Good job");
 		}
 	}
@@ -82,13 +81,13 @@ public class InlineConstant {
 	public static void switch_case_under(int paramVariable) {
 		switch (paramVariable) {
 			case 5:
-				System.out.println(INLINE_CONSTANT);
+				System.out.println(5);
 		}
 	}
 
 	public static void do_while_do() {
 		do {
-			System.out.println(INLINE_CONSTANT);
+			System.out.println(5);
 			break;
 		}
 		while (true);
@@ -98,18 +97,18 @@ public class InlineConstant {
 		do {
 			System.out.println("Good job");
 		}
-		while (INLINE_CONSTANT == 5);
+		while (5 == 5);
 	}
 
 	public static void for_cond() {
-		for (int i = 0; i < INLINE_CONSTANT; i++) {
+		for (int i = 0; i < 5; i++) {
 			System.out.println("Good job");
 		}
 	}
 
 	public static void for_under() {
 		for (int i = 0; i < 5; i++) {
-			System.out.println(INLINE_CONSTANT);
+			System.out.println(5);
 		}
 	}
 
@@ -121,15 +120,15 @@ public class InlineConstant {
 
 	public static void for_each_under() {
 		for (int i : INT_ARRAY) {
-			System.out.println(INLINE_CONSTANT + i);
+			System.out.println(5 + i);
 		}
 	}
 
 	public static void nested_loop() {
 		for (int i : INT_ARRAY) {
-			while(INLINE_CONSTANT == 5) {
-				for (int j = 0; j < INLINE_CONSTANT; j++) {
-					if (j == INLINE_CONSTANT) {
+			while(5 == 5) {
+				for (int j = 0; j < 5; j++) {
+					if (j == 5) {
 						System.out.println("Good job");
 					}
 				}
