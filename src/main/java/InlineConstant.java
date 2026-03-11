8e2c919e65824270748bb599b3008bbf96d2ffb1
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-03-11 15:40:23 +0100

    Ready Inline Constant

---
+++ src/main/java/InlineConstant.java
@@ -0,0 +1,140 @@
+public class InlineConstant {
+	final static int INLINE_CONSTANT = 5;
+	final static int[] INT_ARRAY = new int[5];
+
+
+	public static void min() {
+		System.out.println(INLINE_CONSTANT);
+	}
+
+	public static void ifCond() {
+		if (INLINE_CONSTANT == 5) {
+			System.out.println("Good job");
+		}
+	}
+
+	public static void ifCondUnder() {
+		if (true) {
+			System.out.println(INLINE_CONSTANT);
+		}
+	}
+
+
+	public static void if_else_if_cond(int paramVariable) {
+		if ((INLINE_CONSTANT + paramVariable) == 5) {
+			System.out.println("Good job");
+		} else if(INLINE_CONSTANT == 5) {
+			System.out.println("Bad job");
+		}
+	}
+
+	public static void if_else_if_under(int paramVariable) {
+		if (false) {
+			System.out.println("Good job");
+		} else if(true) {
+			System.out.println(INLINE_CONSTANT);
+		}
+	}
+
+	public static void if_else_cond(int paramVariable) {
+		if (INLINE_CONSTANT == 5) {
+			System.out.println("Good job");
+		} else {
+			System.out.println("Bad job");
+		}
+	}
+
+	public static void if_else_under(int paramVariable) {
+		if (false) {
+			System.out.println("Good job");
+		} else {
+			System.out.println(INLINE_CONSTANT);
+		}
+	}
+
+	public static void while_cond() {
+		while (INLINE_CONSTANT == 5) {
+			System.out.println("Good job");
+		}
+	}
+
+	public static void while_under() {
+		while (true) {
+			System.out.println(INLINE_CONSTANT);
+			break;
+		}
+	}
+
+	public static void switch_cond() {
+		switch (INLINE_CONSTANT) {
+			case 5:
+				System.out.println("Good job");
+		}
+	}
+
+	public static void switch_case_cond(int paramVariable) {
+		switch (paramVariable) {
+			case INLINE_CONSTANT:
+				System.out.println("Good job");
+		}
+	}
+
+	public static void switch_case_under(int paramVariable) {
+		switch (paramVariable) {
+			case 5:
+				System.out.println(INLINE_CONSTANT);
+		}
+	}
+
+	public static void do_while_do() {
+		do {
+			System.out.println(INLINE_CONSTANT);
+			break;
+		}
+		while (true);
+	}
+
+	public static void do_while_cond() {
+		do {
+			System.out.println("Good job");
+		}
+		while (INLINE_CONSTANT == 5);
+	}
+
+	public static void for_cond() {
+		for (int i = 0; i < INLINE_CONSTANT; i++) {
+			System.out.println("Good job");
+		}
+	}
+
+	public static void for_under() {
+		for (int i = 0; i < 5; i++) {
+			System.out.println(INLINE_CONSTANT);
+		}
+	}
+
+	public static void for_each_cond() {
+		for (int i : INT_ARRAY) {
+			System.out.println("Good job");
+		}
+	}
+
+	public static void for_each_under() {
+		for (int i : INT_ARRAY) {
+			System.out.println(INLINE_CONSTANT + i);
+		}
+	}
+
+	public static void nested_loop() {
+		for (int i : INT_ARRAY) {
+			while(INLINE_CONSTANT == 5) {
+				for (int j = 0; j < INLINE_CONSTANT; j++) {
+					if (j == INLINE_CONSTANT) {
+						System.out.println("Good job");
+					}
+				}
+			}
+		}
+	}
+
+}
