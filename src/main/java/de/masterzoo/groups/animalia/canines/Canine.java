5f09468f06742097f4d9c99cb56ad9a71dbe9f8a
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-03-18 14:37:46 +0100

    Ready Inline Superclass (Inline to Anonymous Class):
    - add some javadoc

--- src/main/java/de/masterzoo/groups/animalia/canines/Canine.java
+++ src/main/java/de/masterzoo/groups/animalia/canines/Canine.java
@@ -9,8 +9,15 @@ import de.masterzoo.groups.animalia.reproduction.ReproductionStrategy;
 
 import java.util.ArrayList;
 
+/**
+ * Superclass for all sorts of canines. So far only special in that its constructor is more specificied to avoid
+ * repetition for other canines.
+ */
 public abstract class Canine extends Animal {
 
+	/**
+	 * All canines have the walk movement and 4 legs.
+	 */
 	public Canine(Gender GENDER, String name, int age, int speed, int weight, ReproductionStrategy reproductionStrategy) {
 		ArrayList<MovementStrategy> canineMovementStrategies = new ArrayList<>() {{new Walk();}};
 		int canineNumberOfLegs = 4;
