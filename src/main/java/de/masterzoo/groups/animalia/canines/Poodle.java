e686c08bd7f427a17e661410a61c15d942d7d64e
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-03-18 16:22:46 +0100

    Inline Superclass #4: Move JavaDoc for Inlined Members

--- src/main/java/de/masterzoo/groups/animalia/canines/Poodle.java
+++ src/main/java/de/masterzoo/groups/animalia/canines/Poodle.java
@@ -1,16 +1,20 @@
 package de.masterzoo.groups.animalia.canines;
 
 import de.Gender;
+import de.masterzoo.groups.animalia.Animal;
 import de.masterzoo.groups.animalia.movement.MovementStrategy;
+import de.masterzoo.groups.animalia.movement.Walk;
 import de.masterzoo.groups.animalia.reproduction.ReproductionStrategy;
 
 import java.util.ArrayList;
 
-public class Poodle extends Canine implements Trainable {
+public class Poodle extends Animal implements Trainable {
 
 
 	public Poodle(Gender GENDER, String name, int age, int speed, int weight, ReproductionStrategy reproductionStrategy) {
-		super(GENDER, name, age, speed, weight, reproductionStrategy);
+		ArrayList<MovementStrategy> canineMovementStrategies = new ArrayList<>() {{new Walk();}};
+		int canineNumberOfLegs = 4;
+		super(canineNumberOfLegs, GENDER, name, age, speed, weight, reproductionStrategy, canineMovementStrategies);
 	}
 
 	@Override
