0ec645579de16677f992df3e55b146acc875e6cd
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-03-19 15:13:32 +0100

    Ready Inline Parameter:
    - create two classes with a few usages of a method
    - the argument given to the method in its usages must be the same to be able to execute the inline refactoring
    
    - also ready a special case where field is used as the constant argument

---
+++ src/main/java/inlines/inlineparameter/UsageOutsideClass.java
@@ -0,0 +1,11 @@
+package inlines.inlineparameter;
+
+public class UsageOutsideClass {
+
+	void usageHere3() {
+		InlineParameter iP = new InlineParameter();
+
+		// inlineHere(CONSTANT);
+		iP.inlineHere("CONSTANT");
+	}
+}
