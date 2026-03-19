acc25594740bd6faf4d2fe174217d14f951188bd
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-03-19 15:32:31 +0100

    Inline Parameter #6: Special: Make param a varargs: Replace with local variable

--- src/main/java/inlines/inlineparameter/InlineParameter.java
+++ src/main/java/inlines/inlineparameter/InlineParameter.java
@@ -5,18 +5,19 @@ public class InlineParameter {
 	// Uncomment this for special case
 	// final static String CONSTANT = "CONSTANT_TO_INLINE";
 
-	void inlineHere(String... inlineThis) {
+	void inlineHere() {
+		String[] inlineThis = new String[]{"CONSTANT"};
 		System.out.println(inlineThis);
 	}
 
 	void usageHere() {
 		// inlineHere(CONSTANT);
-		inlineHere("CONSTANT");
+		inlineHere();
 	}
 
 	void usageHere2() {
 		// inlineHere(CONSTANT);
-		inlineHere("CONSTANT");
+		inlineHere();
 	}
 
 }
