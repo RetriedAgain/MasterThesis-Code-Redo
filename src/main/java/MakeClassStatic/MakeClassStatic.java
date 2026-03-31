0744c8464ed93950a0f1fa917d01ed9fc6e7a911
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-03-31 16:24:41 +0200

    Ready Make Class Static #3/4
    - add constructor to A using an upper-class-instance

--- src/main/java/MakeClassStatic/MakeClassStatic.java
+++ src/main/java/MakeClassStatic/MakeClassStatic.java
@@ -3,17 +3,25 @@ package MakeClassStatic;
 public class MakeClassStatic {
 	boolean field_bool_Upper = true;
 
-	A a_upper = new A();
+	A a_upper;
+
+	public MakeClassStatic() {
+	}
 
 	void method_upper() {
 		a_upper.method_inner();
 	}
 
 	public class A {
+		private MakeClassStatic outer;
 		boolean field_bool_lower = true;
 
+		public A(MakeClassStatic outer) {
+			this.outer = outer;
+		}
+
 		// Uncomment this for an extra option
-//		A a = new A();
+		A a = new A(outer);
 
 		public void method_inner() {
 			System.out.println("Test");
--- src/main/java/MakeClassStatic/MakeClassStaticUsage.java
+++ src/main/java/MakeClassStatic/MakeClassStaticUsage.java
@@ -2,6 +2,5 @@ package MakeClassStatic;
 
 public class MakeClassStaticUsage {
 	MakeClassStatic m_outside = new MakeClassStatic();
-	// Uncomment this when selecting "Yes" to replace instance qualifiers with class references. Otherwise causes error.
-	//	MakeClassStatic.A a_outside = m_outside.new A();
+	MakeClassStatic.A a_outside = m_outside.new A(m_outside);
 }
