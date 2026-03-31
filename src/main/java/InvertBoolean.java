32871eec2d9bbe7759a4d6bdc99ae34d78d439f9
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-03-31 14:49:44 +0200

    Invert Boolean #3: Invert Static Boolean Field: Keep name

--- src/main/java/InvertBoolean.java
+++ src/main/java/InvertBoolean.java
@@ -2,7 +2,7 @@ public class InvertBoolean {
 
 //	boolean boolean_field = true;
 	// Same field but static, comment out in test case together with outside usage.
-		static boolean boolean_field = true;
+		static boolean boolean_field = false;
 
 	boolean[] booleans = new boolean[5];
 
@@ -28,26 +28,26 @@ public class InvertBoolean {
 
 
 	public void min() {
-		System.out.println(boolean_field);
+		System.out.println(!boolean_field);
 	}
 
 	public void ifCond() {
-		if (boolean_field) {
+		if (!boolean_field) {
 			System.out.println("Good job");
 		}
 	}
 
 	public  void ifCondUnder() {
 		if (true) {
-			System.out.println(boolean_field);
+			System.out.println(!boolean_field);
 		}
 	}
 
 
 	public  void if_else_if_cond(int paramVariable) {
-		if (boolean_field) {
+		if (!boolean_field) {
 			System.out.println("Good job");
-		} else if(!boolean_field) {
+		} else if(boolean_field) {
 			System.out.println("Bad job");
 		}
 	}
@@ -56,12 +56,12 @@ public class InvertBoolean {
 		if (false) {
 			System.out.println("Good job");
 		} else if(true) {
-			System.out.println(boolean_field);
+			System.out.println(!boolean_field);
 		}
 	}
 
 	public  void if_else_cond(int paramVariable) {
-		if (boolean_field) {
+		if (!boolean_field) {
 			System.out.println("Good job");
 		} else {
 			System.out.println("Bad job");
@@ -72,25 +72,25 @@ public class InvertBoolean {
 		if (false) {
 			System.out.println("Good job");
 		} else {
-			System.out.println(boolean_field);
+			System.out.println(!boolean_field);
 		}
 	}
 
 	public  void while_cond() {
-		while (boolean_field) {
+		while (!boolean_field) {
 			System.out.println("Good job");
 		}
 	}
 
 	public  void while_under() {
 		while (true) {
-			System.out.println(boolean_field);
+			System.out.println(!boolean_field);
 			break;
 		}
 	}
 
 	public  void switch_cond() {
-		switch (boolean_field ? 1: 0) {
+		switch (!boolean_field ? 1: 0) {
 			case 1:
 				System.out.println("Good job");
 		}
@@ -107,13 +107,13 @@ public class InvertBoolean {
 	public  void switch_case_under(int paramVariable) {
 		switch (paramVariable) {
 			case 5:
-				System.out.println(boolean_field);
+				System.out.println(!boolean_field);
 		}
 	}
 
 	public  void do_while_do() {
 		do {
-			System.out.println(boolean_field);
+			System.out.println(!boolean_field);
 			break;
 		}
 		while (true);
@@ -123,18 +123,18 @@ public class InvertBoolean {
 		do {
 			System.out.println("Good job");
 		}
-		while (boolean_field);
+		while (!boolean_field);
 	}
 
 	public  void for_cond() {
-		for (int i = 0; boolean_field; i++) {
+		for (int i = 0; !boolean_field; i++) {
 			System.out.println("Good job");
 		}
 	}
 
 	public  void for_under() {
 		for (int i = 0; i < 5; i++) {
-			System.out.println(boolean_field);
+			System.out.println(!boolean_field);
 		}
 	}
 
@@ -146,15 +146,15 @@ public class InvertBoolean {
 
 	public  void for_each_under() {
 		for (boolean b : booleans) {
-			System.out.println(boolean_field);
+			System.out.println(!boolean_field);
 		}
 	}
 
 	public  void nested_loop() {
 		for (boolean b : booleans) {
-			while(boolean_field) {
-				for (int j = 0; boolean_field; j++) {
-					if (boolean_field) {
+			while(!boolean_field) {
+				for (int j = 0; !boolean_field; j++) {
+					if (!boolean_field) {
 						System.out.println("Good job");
 					}
 				}
