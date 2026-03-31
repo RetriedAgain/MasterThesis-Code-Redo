d2f504f4b4d129c85e7ff7bb1faacefc242c4881
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-03-31 15:59:19 +0200

    Ready Make Class Static:
    - create new class MakeClassStatic with inner class A
    - create usage of upper and inner class in another class

---
+++ src/main/java/MakeClassStatic/MakeClassStatic.java
@@ -0,0 +1,23 @@
+package MakeClassStatic;
+
+public class MakeClassStatic {
+	boolean field_bool_Upper = true;
+
+	A a_upper = new A();
+
+	void method_upper() {
+		a_upper.method_inner();
+	}
+
+	public class A {
+		boolean field_bool_lower = true;
+
+		// Uncomment this for an extra option
+//		A a = new A();
+
+		public void method_inner() {
+			System.out.println("Test");
+		}
+
+	}
+}
---
+++ src/main/java/MakeClassStatic/MakeClassStaticUsage.java
@@ -0,0 +1,7 @@
+package MakeClassStatic;
+
+public class MakeClassStaticUsage {
+	MakeClassStatic m_outside = new MakeClassStatic();
+	// Uncomment this when selecting "Yes" to replace instance qualifiers with class references. Otherwise causes error.
+	//	MakeClassStatic.A a_outside = m_outside.new A();
+}
