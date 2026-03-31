a397abb4711ca28ec9f6c005cf80d603ca66dd0c
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-03-31 14:49:13 +0200

    Ready Invert Boolean #3&4: Invert Static Boolean Field: Keep name

--- src/main/java/InvertBooleanUsages.java
+++ src/main/java/InvertBooleanUsages.java
@@ -1,7 +1,7 @@
 public class InvertBooleanUsages {
 	InvertBoolean inv = new InvertBoolean();
-	boolean usageFieldOutside = inv.boolean_field;
-//	boolean usageStaticFieldOutside = InvertBoolean.boolean_field;
+//	boolean usageFieldOutside = inv.boolean_field;
+	boolean usageStaticFieldOutside = InvertBoolean.boolean_field;
 	boolean usageMethodNonStaticOutside = inv.methodToInvert1();
 	boolean usageMethodStaticOutside = InvertBoolean.methodToInvert2();
 }
