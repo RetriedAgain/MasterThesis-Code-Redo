84bbc1a110c599da19938392f19757f6431e4107
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-04-01 12:18:56 +0200

    Make Method Static #13: Mix: Dialog 2 (when using field in method): Add object as a parameter with name: custom name; Add parameters for fields: Select both and retype and rename and reorder both; Delegate via overloading method

--- src/main/java/makestatic/makemethodstatic/MakeMethodStatic.java
+++ src/main/java/makestatic/makemethodstatic/MakeMethodStatic.java
@@ -9,7 +9,11 @@ public class MakeMethodStatic {
 	}
 
 	public void methodDialog2(String string) {
-		System.out.println("To Be Static or not to be + " + i + j + string);
+		methodDialog2(this, j, i, string);
+	}
+
+	public static void methodDialog2(MakeMethodStatic secret1, int secret3, int secret2, String string) {
+		System.out.println("To Be Static or not to be + " + secret2 + secret3 + string);
 	}
 
 	public void useMethodsInside() {
