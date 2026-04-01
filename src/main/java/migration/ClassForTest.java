ed8ab24af668dde0700ea94c20647bf495a462e8
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-04-01 15:39:10 +0200

    Migrate (Packages and Classes) #2: Migrate package
    - created simple migration mapping that should have renamed the old package migration -> migrationmigrated.
    
    Only did that with the package usage inside the using class though (i.e. within ClassForTest, all the way at the top the package in which it is was renamed to migrationmigrated, but the actual package wasn't renamed!)

--- src/main/java/migration/ClassForTest.java
+++ src/main/java/migration/ClassForTest.java
@@ -1,4 +1,4 @@
-package migration;
+package migrationmigrated;
 
 public class ClassForTest {
 
--- src/test/java/ClassForTestTest.java
+++ src/test/java/ClassForTestTest.java
@@ -1,6 +1,6 @@
 import static org.junit.Assert.*;
 
-import migration.ClassForTest;
+import migrationmigrated.ClassForTest;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
