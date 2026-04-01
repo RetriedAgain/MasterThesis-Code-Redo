70d428873bcb5df898a78610eeeba62f753fe069
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-04-01 11:49:57 +0200

    Make Method Static #1: Dialog 1 (when not using field in method): Replace instance qualifiers with class references

--- src/main/java/makestatic/makemethodstatic/UseMakeMethodStatic.java
+++ src/main/java/makestatic/makemethodstatic/UseMakeMethodStatic.java
@@ -5,7 +5,7 @@ public class UseMakeMethodStatic {
 	MakeMethodStatic makeMethodStatic = new MakeMethodStatic();
 
 	public void useMethodsOutside() {
-		makeMethodStatic.methodDialog1();
+		MakeMethodStatic.methodDialog1();
 		makeMethodStatic.methodDialog2("Hi");
 	}
 }
