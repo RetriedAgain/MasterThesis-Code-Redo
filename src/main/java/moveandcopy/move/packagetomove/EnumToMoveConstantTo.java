f8d255b802d311dce047637bfdd9bf80f369fe52
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-04-02 15:55:59 +0200

    Move and Copy: Move Static Members (including Method) #5: Mix: Move Constant as Enum Constant and Static Method to Enum, set vis of both to package local

--- src/main/java/moveandcopy/move/packagetomove/EnumToMoveConstantTo.java
+++ src/main/java/moveandcopy/move/packagetomove/EnumToMoveConstantTo.java
@@ -1,10 +1,15 @@
 package moveandcopy.move.packagetomove;
 
 public enum EnumToMoveConstantTo {
-	;
+	CONSTANT_TO_MOVE(1);
 
 	int value;
 	EnumToMoveConstantTo(int value) {
 		this.value = value;
+	}
+
+	static void staticMethodToMove() {
+		System.out.println("Move this");
+		System.out.println("Add the constant in here for good measure + " + CONSTANT_TO_MOVE);
 	}
 }
