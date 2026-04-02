40279e1f22146325e4fa17331cd25d5c006a5d9d
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-04-02 14:20:31 +0200

    Move and Copy: Move Instance Method #4: Mix: Visiblity to Package Local and give custom name to param

--- src/main/java/moveandcopy/move/packagetomove/TypeExclusiveToProject.java
+++ src/main/java/moveandcopy/move/packagetomove/TypeExclusiveToProject.java
@@ -1,4 +1,10 @@
 package moveandcopy.move.packagetomove;
 
 public class TypeExclusiveToProject {
+	void instanceMethodToMove(TypeExclusiveToProject2 typeExclusiveToProject2, ClassToMove secret) {
+		System.out.println("Move this");
+		System.out.println("fieldToMove = " + secret.fieldToMove);
+		System.out.println("Type1 = " + this);
+		System.out.println("Type2 = " + typeExclusiveToProject2);
+	}
 }
