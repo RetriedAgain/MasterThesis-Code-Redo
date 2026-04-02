82fa331e98ec8eedd0b2a50505065b1b19e9d514
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-04-02 15:53:57 +0200

    Move and Copy: Move Static Members (including Method) #2: Move constant to Enum, set Vis to package local

--- src/main/java/moveandcopy/move/packagetomove/EnumToMoveConstantTo.java
+++ src/main/java/moveandcopy/move/packagetomove/EnumToMoveConstantTo.java
@@ -3,6 +3,9 @@ package moveandcopy.move.packagetomove;
 public enum EnumToMoveConstantTo {
 	;
 
+	// Movable through menu Move Static Members
+	// Can be moved to Enum as Enum Constant, too, because the Enum EnumToMoveConstantTo has a fitting constructor
+	final static int CONSTANT_TO_MOVE = 1;
 	int value;
 	EnumToMoveConstantTo(int value) {
 		this.value = value;
