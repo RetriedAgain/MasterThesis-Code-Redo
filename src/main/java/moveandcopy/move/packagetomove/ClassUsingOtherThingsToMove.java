42293c82e14fafc33dfa2b8ff54f43292ec5f667
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-04-03 15:47:54 +0200

    Move and Copy: Move Class #2: Move Class under current package

--- src/main/java/moveandcopy/move/packagetomove/ClassUsingOtherThingsToMove.java
+++ src/main/java/moveandcopy/move/packagetomove/ClassUsingOtherThingsToMove.java
@@ -1,5 +1,7 @@
 package moveandcopy.move.packagetomove;
 
+import moveandcopy.move.packagetomove.lowerpackage.ClassToMove;
+
 public class ClassUsingOtherThingsToMove {
 
 	ClassToMove classToMove;
--- src/main/java/moveandcopy/move/packagetomove/lowerpackage/ClassToMove.java
+++ src/main/java/moveandcopy/move/packagetomove/lowerpackage/ClassToMove.java
@@ -1,4 +1,7 @@
-package moveandcopy.move.packagetomove;
+package moveandcopy.move.packagetomove.lowerpackage;
+
+import moveandcopy.move.packagetomove.TypeExclusiveToProject;
+import moveandcopy.move.packagetomove.TypeExclusiveToProject2;
 
 public class ClassToMove {
 
