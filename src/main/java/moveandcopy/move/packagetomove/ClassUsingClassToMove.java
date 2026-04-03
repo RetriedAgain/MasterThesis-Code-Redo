1ea703c433484d46a29600cbc921a277091d8ede
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-04-03 11:00:52 +0200

    Ready Move Inner Class to Upper Level:
    - ready an inner class and inner inner class with one field, constant and methods (static and non-static) being used outside the file once each
    
    - adjust all things to public so no errors appear when moving upper-inner to diff. package.

--- src/main/java/moveandcopy/move/packagetomove/ClassUsingClassToMove.java
+++ src/main/java/moveandcopy/move/packagetomove/ClassUsingClassToMove.java
@@ -3,10 +3,37 @@ package moveandcopy.move.packagetomove;
 public class ClassUsingClassToMove {
 
 	ClassToMove classToMove;
+	ClassToMove.InnerClassToMove innerClassToMove;
+	ClassToMove.InnerClassToMove.InnerInnerClassToMove innerInnerClassToMove;
 
-	public ClassUsingClassToMove(ClassToMove classToMove) {
+	public ClassUsingClassToMove(ClassToMove classToMove, ClassToMove.InnerClassToMove innerClassToMove,
+								 ClassToMove.InnerClassToMove.InnerInnerClassToMove innerInnerClassToMove) {
 		this.classToMove = classToMove;
+		this.innerClassToMove = innerClassToMove;
+		this.innerInnerClassToMove = innerInnerClassToMove;
 	}
+
+	public void useFieldsOfInnerClassToMove(ClassToMove.InnerClassToMove innerClassToMove) {
+		System.out.println(innerClassToMove.innerFieldToMove);
+		System.out.println(ClassToMove.InnerClassToMove.INNER_CONSTANT_TO_MOVE);
+	}
+
+	public void useMethodsOfInnerClassToMove() {
+		innerClassToMove.innerMethodToMove();
+		ClassToMove.InnerClassToMove.innerStaticMethodToMove();
+	}
+
+	public void useMethodsOfInnerInnerClassToMove() {
+		innerInnerClassToMove.innerInnerMethodToMove();
+		ClassToMove.InnerClassToMove.InnerInnerClassToMove.innerInnerStaticMethodToMove();
+	}
+
+	public void useFieldsOfInnerInnerClassToMove(ClassToMove.InnerClassToMove.InnerInnerClassToMove innerInnerClassToMove) {
+		System.out.println(innerInnerClassToMove.innerInnerFieldToMove);
+		System.out.println(ClassToMove.InnerClassToMove.InnerInnerClassToMove.INNER_INNER_CONSTANT_TO_MOVE);
+	}
+
+
 
 	public void useInstanceMethod() {
 		classToMove.instanceMethodToMove(new TypeExclusiveToProject(), new TypeExclusiveToProject2());
