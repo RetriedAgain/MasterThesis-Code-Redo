108d61e446645c0deeafdc47edcc2570b2c2ca68
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-04-03 13:55:38 +0200

    Move Inner Class to Upper Level #6: Move nested inner class to upper hierarchy level: Keep class name, do not pass outer class’ instance as a parameter

--- src/main/java/moveandcopy/move/packagetomove/ClassToMove.java
+++ src/main/java/moveandcopy/move/packagetomove/ClassToMove.java
@@ -47,22 +47,21 @@ public class ClassToMove {
 			System.out.println("innerConstantToMove = " + INNER_CONSTANT_TO_MOVE);
 		}
 
-		public class InnerInnerClassToMove {
-			public int innerInnerFieldToMove;
-			public static int INNER_INNER_CONSTANT_TO_MOVE;
-
-			public void innerInnerMethodToMove() {
-				System.out.println("Inner-inner Method here.");
-				System.out.println("innerInnerFieldToMove = " + innerInnerFieldToMove);
-			}
-
-			public static void innerInnerStaticMethodToMove() {
-				System.out.println("Inner-inner Method here.");
-				System.out.println(INNER_INNER_CONSTANT_TO_MOVE);
-			}
-		}
-
 	}
 
 
+	public class InnerInnerClassToMove {
+		public int innerInnerFieldToMove;
+		public static int INNER_INNER_CONSTANT_TO_MOVE;
+
+		public void innerInnerMethodToMove() {
+			System.out.println("Inner-inner Method here.");
+			System.out.println("innerInnerFieldToMove = " + innerInnerFieldToMove);
+		}
+
+		public static void innerInnerStaticMethodToMove() {
+			System.out.println("Inner-inner Method here.");
+			System.out.println(INNER_INNER_CONSTANT_TO_MOVE);
+		}
+	}
 }
