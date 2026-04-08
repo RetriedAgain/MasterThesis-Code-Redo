a8a197e5d03f99b2e9b24931f95e7288c3edfac0
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-04-08 14:00:59 +0200

    Pull Members Up: #1: Def: Pull up Field and method from class to super class, ignore JavaDoc options from hereon

--- src/main/java/pullmembersup/ClassToPullMembersUpFrom.java
+++ src/main/java/pullmembersup/ClassToPullMembersUpFrom.java
@@ -2,12 +2,6 @@ package pullmembersup;
 
 public class ClassToPullMembersUpFrom extends SuperclassToPullMembersInto {
 
-	int fieldToPullUp;
-
-	public void methodToPullUp() {
-		System.out.println("ClassToPullMembersUpFrom.methodToPullUp");
-	}
-
 	public static void staticMethodToPullUp() {
 		System.out.println("ClassToPullMembersUpFrom.staticMethodToPullUp");
 	}
--- src/main/java/pullmembersup/SuperclassToPullMembersInto.java
+++ src/main/java/pullmembersup/SuperclassToPullMembersInto.java
@@ -2,9 +2,13 @@ package pullmembersup;
 
 public class SuperclassToPullMembersInto {
 	public static final int SUPERCLASS_CONSTANT = 99;
+    int fieldToPullUp;
 
-	public void superclassMethod() {
+    public void superclassMethod() {
 		System.out.println("SuperclassToPullMembersInto.superclassMethod()");
 	}
 
+    public void methodToPullUp() {
+        System.out.println("ClassToPullMembersUpFrom.methodToPullUp");
+    }
 }
