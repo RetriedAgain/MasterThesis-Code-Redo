1ced7cb5a0c99c49ec931e3dee27b8f56e83e7e4
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-04-20 15:03:51 +0200

    Replace Inheritance With Delegation #2: Do not generate getter for delegated component
    - casues error because of lack of access as well. But because def case passes, this will not be fixed.

--- src/main/java/replaceinheritancewithdelegation/MiddleClass.java
+++ src/main/java/replaceinheritancewithdelegation/MiddleClass.java
@@ -1,10 +1,15 @@
 package replaceinheritancewithdelegation;
 
-public class MiddleClass extends UpperClass {
+public class MiddleClass {
+	private final UpperClass upperClass = new UpperClass();
 	public int middleField = 1;
 	public final static int MIDDLE_CONSTANT = 0;
 
 	public void useUpperClass() {
-		System.out.println(upperField);
+		System.out.println(upperClass.upperField);
+	}
+
+	public void useUpperAttributes() {
+		upperClass.useUpperAttributes();
 	}
 }
