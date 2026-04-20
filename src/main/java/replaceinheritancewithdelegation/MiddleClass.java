754c292ac59967ad64ac991da77a05edbf838985
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-04-20 15:02:54 +0200

    Replace Inheritance With Delegation #1: Def: Replace with delegation instance from superclass, leave field name as is suggested, delegate one method, generate getter for delegated component

--- src/main/java/replaceinheritancewithdelegation/MiddleClass.java
+++ src/main/java/replaceinheritancewithdelegation/MiddleClass.java
@@ -1,10 +1,19 @@
 package replaceinheritancewithdelegation;
 
-public class MiddleClass extends UpperClass {
+public class MiddleClass {
+	private final UpperClass upperClass = new UpperClass();
 	public int middleField = 1;
 	public final static int MIDDLE_CONSTANT = 0;
 
 	public void useUpperClass() {
-		System.out.println(upperField);
+		System.out.println(upperClass.upperField);
+	}
+
+	public UpperClass getUpperClass() {
+		return upperClass;
+	}
+
+	public void useUpperAttributes() {
+		upperClass.useUpperAttributes();
 	}
 }
