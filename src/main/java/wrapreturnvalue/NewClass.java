29aa36d415f4cad30a0f0aab08c5f0d56488f64a
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-04-22 16:20:01 +0200

    Ready Wrap Return Value #4: Special: Only wrap 1 return value

--- src/main/java/wrapreturnvalue/NewClass.java
+++ src/main/java/wrapreturnvalue/NewClass.java
@@ -4,12 +4,7 @@ class NewClass {
 
 	int methodOfReturnValueToBeWrapped() {
 		int wrapperField = 1;
-
-		if (wrapperField == 2) {
-			return wrapperField;
- 		} else {
-			return wrapperField * 2;
-		}
+		return wrapperField;
 	}
 
 
