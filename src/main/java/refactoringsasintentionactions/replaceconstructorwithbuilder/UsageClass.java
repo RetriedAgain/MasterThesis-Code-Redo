d03368496d44b08ff8849939d2340a4868cd7ac9
Ramon S. <57843146+RetriedAgain@users.noreply.github.com>
2026-04-28 16:38:54 +0200

    Replace Constructor With Builder #1: Def: 3 params to pass to builder (int, String, int varargs) with field names kept as they are, setter names as suggested, default values empty, optional setters not selected; Create New with default builder class name, same package for new builder and leave in same source root

--- src/main/java/refactoringsasintentionactions/replaceconstructorwithbuilder/UsageClass.java
+++ src/main/java/refactoringsasintentionactions/replaceconstructorwithbuilder/UsageClass.java
@@ -2,7 +2,7 @@ package refactoringsasintentionactions.replaceconstructorwithbuilder;
 
 public class UsageClass {
 	public static void main(String[] args) {
-		ClassWithConstructorToHide c = new ClassWithConstructorToHide(5, "Test", 3, 4);
+		ClassWithConstructorToHide c = new ClassWithConstructorToHideBuilder().setA(5).setB("Test").setVarArgC(3, 4).createClassWithConstructorToHide();
 		System.out.println(c);
 	}
 }
